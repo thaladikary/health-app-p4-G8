224f14aec9413666ba1f3d536e91ffe67c37b5f1
Michael <mtand920@mtroyal.ca>
2023-11-09 03:43:51 -0700

    barcode working

--- src/screens/main/Scanner.js
+++ src/screens/main/Scanner.js
@@ -1,57 +1,55 @@
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Navbar from "../../components/Navbar"
+import axios from 'axios';
+export default function Scanner({navigation}) {
+    const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-export default function App() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
 
-  if (!permission) {
-    // Camera permissions are still loading
-    return <View />;
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    console.log(data)
+    axios.get("https://world.openfoodfacts.net/api/v2/product/8000500289495")
+    .then(resp=> console.log(resp.data.product.image_url))
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
   }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
-
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
-
+  
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={CameraType.back}>
-        {/* <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View> */}
-      </Camera>
+      <BarCodeScanner style={styles.camera} onBarCodeScanned={handleBarCodeScanned}/>
+      <Navbar navigation={navigation}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
     justifyContent: 'center',
   },
   camera: {
-    flex: 1,
+    width:500,
+    height:680
   },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
+ 
   button: {
     flex: 1,
     alignSelf: 'flex-end',
