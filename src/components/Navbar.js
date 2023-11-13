c1b187b171b2044bfd28382ecf7ab5021be33d8f
Thal Adikary <thaladikary@gmail.com>
2023-11-13 01:46:15 -0700

    started navbar design/functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,73 +1,89 @@
-import { View, Text, StyleSheet,Image, Dimensions, StatusBar, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const screenWidth = Dimensions.get('window').width
-const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const scaleFactor = 0.05;
 const marginValue = screenWidth * scaleFactor;
-export default function Navbar({ navigation }){
+export default function Navbar({ navigation }) {
+  const handleTrackCalories = () => {
+    navigation.navigate("TrackCalories");
+  };
+  const handleMaps = () => {
+    navigation.navigate("Maps");
+  };
+  const handleAdd = () => {
+    navigation.navigate("");
+  };
 
-    const handleTrackCalories = () =>{
-        navigation.navigate("TrackCalories")
-    }
-    const handleMaps = ()=>{
-        navigation.navigate("Maps")
-    }
-    const handleGroceryList = ()=>{
-        navigation.navigate("GroceryList")
-    }
-    const handleAccount = ()=>{
-        navigation.navigate("Account")
-    }
-    return(
-        <View style={styles.navbarContainer}>
-            <TouchableOpacity onPress={handleTrackCalories}>
-              <View style={styles.labelContainer}>
-                <Text>Track Calories</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleMaps}>
-            <View style={styles.labelContainer}>
-                <Text>Maps</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleGroceryList}>
-            <View style={styles.labelContainer}>
-                <Text>Grocery</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleAccount}>
-            <View style={styles.labelContainer}>
-                <Text>Profile</Text>
-              </View>
-            </TouchableOpacity>
+  const handleGroceryList = () => {
+    navigation.navigate("GroceryList");
+  };
+  const handleAccount = () => {
+    navigation.navigate("Account");
+  };
+
+  return (
+    <View style={styles.navbarContainer}>
+      <TouchableOpacity onPress={handleTrackCalories}>
+        <View style={styles.labelContainer}>
+          <Ionicons name="calendar-outline" size={45} />
         </View>
-    )
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleMaps}>
+        <View style={styles.labelContainer}>
+          <Text>Maps</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleAdd}>
+        <View style={styles.labelContainer}>
+          <Text>+</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleGroceryList}>
+        <View style={styles.labelContainer}>
+          <Text>Grocery</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleAccount}>
+        <View style={styles.labelContainer}>
+          <Text>Profile</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    navbarContainer:{
-        // display:'flex',
-        // flexDirection:"row",
-        // width:screenWidth,
-        // // height:50,
-        borderTopColor:"black",
-        justifyContent: "space-between",
-        borderTopWidth:1,
-        display: "flex",
-        flexDirection: "row",
-        marginTop: marginValue,
-
-    },
-    labelContainer:{
-        // alignContent:"space-between",
-        // margin:10,
-        // marginTop:15,
-        // marginBottom:15,
-        // marginLeft:20
-        borderWidth: 2,        // Border width
-        borderColor: 'red',  // Border color
-        borderRadius: 1,
-
-    }
-})
+  navbarContainer: {
+    // display:'flex',
+    // flexDirection:"row",
+    // width:screenWidth,
+    // // height:50,
+    borderTopColor: "black",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    display: "flex",
+    flexDirection: "row",
+    marginTop: marginValue,
+  },
+  labelContainer: {
+    margin: 10,
+    marginTop: 15,
+    marginBottom: 15,
+    marginLeft: 20,
+    // borderWidth: 2, // Border width
+    // borderColor: "red", // Border color
+    // borderRadius: 1,
+  },
+});
