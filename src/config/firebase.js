a843f5e9f8c11a5002acee35b9e94a721b908405
Michael <michaeltandyo@gmail.com>
2023-11-14 17:55:39 -0700

    changed to env oops

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,14 +8,14 @@ import {FIREBASE_API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDELZBFc8J26Cx7TLdpZyZPOlCls_lxaYc",
-    authDomain: "prog4-healthapp.firebaseapp.com",
-    projectId: "prog4-healthapp",
-    storageBucket: "prog4-healthapp.appspot.com",
-    messagingSenderId: "11934921754",
-    appId: "1:11934921754:web:c17ca7817d604bf3e48da3",
-    measurementId: "G-6R8T7FY5SE"
-  };
+  apiKey: FIREBASE_API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: MEANSUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
