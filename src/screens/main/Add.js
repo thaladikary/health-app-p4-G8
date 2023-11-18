0d3155a117625e63fecc682654c90b8e44aec986
tadik910 <tadik910@mtroyal.ca>
2023-11-18 10:50:25 -0700

    started search-add-page

--- src/screens/main/Add.js
+++ src/screens/main/Add.js
@@ -1,12 +1,85 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, FlatList, StyleSheet } from "react-native";
 import Navbar from "../../components/Navbar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { APP_KEY, APP_ID } from "@env";
 
 export default function Add({ navigation }) {
+  const [query, setQuery] = useState("");
+  const [foodList, setFoodList] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChange = async (input) => {
+    setQuery(input);
+  };
+  const headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+  };
+  const apiUrl = "https://trackapi.nutritionix.com/v2/search/instant";
+
+  axios
+    .get(apiUrl, {
+      params: {
+        query: "common", // An empty query will return all foods
+      },
+      headers: {
+        "x-app-id": APP_ID,
+        "x-app-key": APP_KEY,
+      },
+    })
+    .then((response) => {
+      // Handle successful response
+      setSuggestions(response.data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error fetching data:", error);
+    });
+
+  const renderSuggestionItem = ({ item }) => (
+    <TouchableOpacity style={styles.suggestionItem}>
+      <Text>{item}</Text>
+    </TouchableOpacity>
+  );
   return (
     <View>
-      <Text>Add/search page</Text>
+      <View style={styles.search_container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleInputChange}
+          value={query}
+        />
+      </View>
+      <FlatList
+        data={suggestions}
+        renderItem={renderSuggestionItem}
+        keyExtractor={(item) => item.toString()}
+        style={styles.suggestionsList}
+      />
+
       <Navbar navigation={navigation} />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  search_container: {
+    flex: 1,
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  suggestionsList: {
+    marginTop: 8,
+  },
+  suggestionItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+});
--- src/screens/main/Scanner.js
+++ src/screens/main/Scanner.js
@@ -1,33 +1,40 @@
-import { Camera, CameraType } from 'expo-camera';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View ,Dimensions} from 'react-native';
-import Navbar from "../../components/Navbar"
-import axios from 'axios';
-import { APP_KEY,APP_ID } from '@env';
-const screenWidth = Dimensions.get('window').width
-const screenHeight = Dimensions.get('window').height;
-export default function Scanner({navigation,route}) {
-    const mealType = route.params && route.params.mealType
-    const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(false);
-    const [foodData, setFoodData] = useState()
+import { Camera, CameraType } from "expo-camera";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { useState, useEffect } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from "react-native";
+import Navbar from "../../components/Navbar";
+import axios from "axios";
+import { APP_KEY, APP_ID } from "@env";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+export default function Scanner({ navigation, route }) {
+  const mealType = route.params && route.params.mealType;
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [foodData, setFoodData] = useState();
   useEffect(() => {
-    setScanned(false)
+    setScanned(false);
     const getBarCodeScannerPermissions = async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     };
 
     getBarCodeScannerPermissions();
   }, []);
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       setScanned(false);
       setHasPermission(false);
       (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted"); 
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === "granted");
       })();
     });
     return unsubscribe;
@@ -35,50 +42,45 @@ export default function Scanner({navigation,route}) {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    console.log(data)
-    try{
+    console.log(data);
+    try {
       const headers = {
-        'x-app-id': APP_ID,
-        'x-app-key':APP_KEY
-      }
-      const apiLink = "https://trackapi.nutritionix.com/v2/search/item?upc="
+        "x-app-id": APP_ID,
+        "x-app-key": APP_KEY,
+      };
+      const apiLink = "https://trackapi.nutritionix.com/v2/search/item?upc=";
 
-      axios.get(apiLink+data,{headers})
-      .then(resp=>{
-        const data = resp.data.foods[0]
-        console.log(data.food_name)
-        prop={
+      axios.get(apiLink + data, { headers }).then((resp) => {
+        const data = resp.data.foods[0];
+        console.log(data.food_name);
+        prop = {
           name: data.food_name,
-          image:data.photo.thumb,
-          nutriments:{
-            calories:data.nf_calories,
-            fat:data.nf_total_fat,
-            protein:data.nf_protein,
-            carbohydrates: data.nf_total_carbohydrate
-          }
-          
-        }
-        navigation.navigate("FoodDetails", {prop,mealType});
-      })
+          image: data.photo.thumb,
+          nutriments: {
+            calories: data.nf_calories,
+            fat: data.nf_total_fat,
+            protein: data.nf_protein,
+            carbohydrates: data.nf_total_carbohydrate,
+          },
+        };
+        navigation.navigate("FoodDetails", { prop, mealType });
+      });
       // console.log(headers)
       // axios.get("https://world.openfoodfacts.net/api/v2/product/"+data)
       // .then(resp => {
       //   setFoodData({ image: resp.data.product.image_url });
       //   const data = resp.data
-      //   prop={ 
+      //   prop={
       //     name:data.product.product_name,
-      //     image: data.product.image_url, 
-      //     nutriments: data.product.nutriments, 
-          
+      //     image: data.product.image_url,
+      //     nutriments: data.product.nutriments,
+
       //   }
       //   navigation.navigate("FoodDetails", {prop});
       // })
-
-    }catch(e){
-      console.log("invalid scan")
+    } catch (e) {
+      console.log("invalid scan");
     }
-   
-   
   };
 
   if (hasPermission === null) {
@@ -87,20 +89,18 @@ export default function Scanner({navigation,route}) {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  
+
   return (
     <View style={styles.container}>
-       <View style={styles.overlay}></View>
-      <BarCodeScanner style={styles.camera} onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}>
-        
-        </BarCodeScanner>
-        <View style={styles.box}>
-         
-        </View>
-        <View style={styles.barcodeInstruction}>
-            <Text style={styles.text}>Point your camera at a barcode</Text> 
-        </View>
-       
+      <View style={styles.overlay}></View>
+      <BarCodeScanner
+        style={styles.camera}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      ></BarCodeScanner>
+      <View style={styles.box}></View>
+      <View style={styles.barcodeInstruction}>
+        <Text style={styles.text}>Point your camera at a barcode</Text>
+      </View>
     </View>
   );
 }
@@ -110,31 +110,31 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
-    ...StyleSheet.absoluteFillObject, 
+    ...StyleSheet.absoluteFillObject,
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(0,0,0,0.7)', 
+    backgroundColor: "rgba(0,0,0,0.7)",
   },
   box: {
     borderWidth: 2,
-    borderColor: 'white',
+    borderColor: "white",
     width: 270,
     height: 180,
-    position: 'absolute',
+    position: "absolute",
     zIndex: 2,
-    top: (screenHeight - 180) / 2, 
-    left: (screenWidth - 270) / 2, 
+    top: (screenHeight - 180) / 2,
+    left: (screenWidth - 270) / 2,
     borderRadius: 20,
   },
   barcodeInstruction: {
-    position: 'absolute',
+    position: "absolute",
     top: screenHeight / 1.5,
     left: 40,
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
+    fontWeight: "bold",
+    color: "white",
   },
 });
