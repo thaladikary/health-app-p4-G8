0b86982584b7c00193637ae5c6d1476d81d27ca7
Michael <michaeltandyo@gmail.com>
2023-11-21 03:41:29 -0700

    finished firebase storage for nutrition tracker diary

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -2,14 +2,19 @@ import React from "react"
 import UserStack from "./UserStack"
 import AuthStack from "./AuthStack"
 import{useAuth} from "../hooks/useAuth"
-
+import {UserContext} from "../context/userContext"
 
 export default function RootNavigation() {
     const { user } = useAuth();
     console.log(user)
+    // make sure to not include everything later in this user log because it contains api key
     if(user){
         console.log("login successful")
-        return <UserStack/>
+        return (
+            <UserContext.Provider value={user}>
+                <UserStack/>
+            </UserContext.Provider>
+        )
     }
     return(
         <AuthStack/>
