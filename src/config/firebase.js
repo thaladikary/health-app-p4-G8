868da4c02171a5d154d242fe4bdd18f724afbd3f
Michael <michaeltandyo@gmail.com>
2023-11-23 03:28:35 -0700

    fix auth firebase bug

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "@firebase/app";
-import { getAuth } from "@firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "@firebase/auth";
 import { getFirestore } from '@firebase/firestore';
-import {FIREBASE_API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_ID,FIREBASE_APP_ID,MEANSUREMENT_ID} from "@env"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FIREBASE_API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, FIREBASE_APP_ID, MEASUREMENT_ID } from "@env";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,12 +13,20 @@ const firebaseConfig = {
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
-  measurementId: MEANSUREMENT_ID
+  measurementId: MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const db = getFirestore(app)
 
-export {auth,db}
+// Use getReactNativePersistence to get persistence for React Native
+const authPersistence = getReactNativePersistence(AsyncStorage);
+
+// Initialize Firebase Auth with persistence
+const auth = initializeAuth(app, {
+  persistence: authPersistence
+});
+
+const db = getFirestore(app);
+
+export { auth, db };
