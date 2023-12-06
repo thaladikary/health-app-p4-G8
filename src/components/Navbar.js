249ff5a3de36dd87e4aaa2e88b1a2b74b800df13
Michael <michaeltandyo@gmail.com>
2023-12-06 03:07:27 -0700

    natural language stored to firebase done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,6 +45,7 @@ export default function Navbar({ active, navigation }) {
         <NaturalLanguageSearch
           visible={isModalVisible}
           setVisible={(data) => setVisibleFromChild(data)}
+          navigation={navigation}
         />
       ) : (
         <View></View>
