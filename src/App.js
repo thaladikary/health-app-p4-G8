07020dda621659d937562b602ec6a5694a1e57b2
Michael <michaeltandyo@gmail.com>
2023-12-10 18:39:56 -0700

    updated bmr calc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import RootNavigation from './navigation/RootNavigation';
 import SetupStack from './navigation/SetupStack';
 import AuthStack from './navigation/AuthStack';
+import CalculateBMR from './screens/setup/CalculateBMR';
 
 
 // so its gonna be login/register stack -> if true->
@@ -12,10 +13,10 @@ export default function App() {
   return (
 
         <SafeAreaView style={styles.container}>
-     
-          <SetupStack></SetupStack>
-          
-           {/* <RootNavigation/> */}
+          <CalculateBMR/>
+          {/* <SetupStack></SetupStack> */}
+{/*           
+           <RootNavigation/> */}
        </SafeAreaView>
 
  
